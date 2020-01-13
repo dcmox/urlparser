eaa0dba85b374a1423c809f22113367495170199
dcmox <dancmox@comcast.net>
2020-01-12 19:29:40 -0600

    fix for %

--- tests/usage.js
+++ tests/usage.js
@@ -7,6 +7,6 @@ console.log(urlparser_1.parseUri('https://www.google.com:123'));
 console.log(urlparser_1.parseUri('https:yahoo.com:4000'));
 console.log(urlparser_1.parseUri('https://whatis.techtarget.com/search/query?q=URL'));
 console.log(urlparser_1.parseUri('http:google.com/test/url?param=blah&blah=test#arr'));
-console.log(urlparser_1.parseUri('http:google.com:8000/test/url?param=blah&blah=test#arr'));
+console.log(urlparser_1.parseUri('http:google.com:8000/test/url?param=blah&blah=test%20hmm#arr'));
 console.log(urlparser_1.parseUri('http://www.99ride.com'));
 console.log(urlparser_1.parseUri('ftp://myname@host.com'));
